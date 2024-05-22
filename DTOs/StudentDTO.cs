d07c43960403acaf4327d90ef3ed9ffa8664e6c1
Gustavo <yusgus02@gmail.com>
2024-05-22 13:28:03 -0600

    Agregar metodos Necesarios

---
+++ DTOs/StudentDTO.cs
@@ -0,0 +1,20 @@
+namespace TeamHubServiceUser.DTOs;
+
+public class StudentDTO
+{
+    public int IdStudent { get; set; }
+ 
+    public string? Name { get; set; }
+
+    public string? MiddleName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? SurName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? ProDocumentImage { get; set; }
+}
